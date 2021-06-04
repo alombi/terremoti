b372d18984181a74dc7765f796ae1863f8ad69ee
alombi <alombi@hotmail.com>
2021-06-04 22:59:58 +0200

    Fixes bug with API

--- src/App.svelte
+++ src/App.svelte
@@ -17,7 +17,7 @@
 	let sort;
 	let promise, updatingList;
 	async function getData(){
-		let request = await fetch('https://ingv.alombi.xyz/api/latest')
+		let request = await fetch('https://ingv.alombi.xyz/latest')
 		let response = await request.json()
 		data = response.events
 		events.set(data)
