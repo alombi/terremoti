ee2d3923adbf0ff35b4476e2d72cabb83630a0c2
alombi <alombi@hotmail.com>
2022-04-09 18:37:17 +0200

    Close #21

--- src/lib/components/Map.svelte
+++ src/lib/components/Map.svelte
@@ -1,5 +1,6 @@
 <script>
-	import {LeafletMap, TileLayer, Circle} from 'svelte-leafletjs';
+	const months = ['Gennaio' ,'Febbraio', 'Marzo', 'Aprile', 'Maggio', 'Giugno', 'Luglio', 'Agosto', 'Settembre', 'Ottobre', 'Novembre', 'Dicembre']
+	import { LeafletMap, TileLayer, Circle, Popup } from 'svelte-leafletjs';
     import { events } from '../stores/events';
 
 	const mapOptions = {
@@ -19,14 +20,23 @@
 		orange:'#FF8F33',
 		yellow:'#FEC309'
 	}
+	function tap(event){
+		leafletMap.getMap().flyTo([event.lat, event.long], 10	);
+		radius = 5000
+	}
+	let radius = 9000;
 </script>
 
 <div id="map" style="width: 100%; height: 80%;">
 	<LeafletMap bind:this={leafletMap} options={mapOptions}>
 		 <TileLayer url={tileUrl} options={tileLayerOptions}/>
          {#each $events as event}
-            <Circle latLng={[event.lat, event.long]} radius={7000} color={colors[event.color]} weight="2" fillOpacity="0.6" fillColor={colors[event.color]}>
-            </Circle>
+            <Circle events={['click']} on:click={()=>{tap(event)}} latLng={[event.lat, event.long]} radius={radius} color={colors[event.color]} weight="2" fillOpacity="0.6" fillColor={colors[event.color]}>
+					<Popup>
+						<b>{event.mag} Mg</b> - {event.region}<br>
+						<span>{new Date(event.time).getDate().toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false})} {months[new Date(event.time).getMonth()]} {new Date(event.time).getFullYear()} - {new Date(event.time).getHours().toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false})}:{new Date(event.time).getMinutes().toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false})} - Depth: {Number(event.depth) / 1000} Km</span>
+					</Popup>
+				</Circle>
          {/each}
 	</LeafletMap>
 </div>
--- src/lib/functions/fetchData.js
+++ src/lib/functions/fetchData.js
@@ -4,7 +4,6 @@ export async function getData(isRefresh) {
    const url = 'https://ingv.alombi.xyz/latest'
    let req = await fetch(url)
    let json = await req.json()
-   console.log(json)
    json = json.events
    events.set(json)
    if (isRefresh) {
