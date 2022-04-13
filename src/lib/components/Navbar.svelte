1e54c0d972acbb67de309e7c6865d8069bc4d345
alombi <alombi@hotmail.com>
2022-04-13 23:39:43 +0200

    Add persistency to color scheme (Close #22)

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -4,6 +4,7 @@
    import { refreshed } from '../stores/events';
    import { getNotificationsContext } from 'svelte-notifications';
    const { addNotification } = getNotificationsContext();
+   import { onMount } from 'svelte';
    $:{
       console.log($refreshed)
       if($refreshed){
@@ -11,17 +12,31 @@
          refreshed.set(false)
       }
    }
-   let icon = 'moon'
+   let icon = 'moon';
    function switchMode(){
       if(!document.getElementsByClassName('map-tiles-container')[0].classList.contains('map-tiles')){
          document.getElementsByClassName('map-tiles-container')[0].classList.add('map-tiles')
          icon = 'sun'
+         window.localStorage.setItem('mode', 'moon')
       }else{
          document.getElementsByClassName('map-tiles-container')[0].classList.remove('map-tiles')
          icon = 'moon'
+         window.localStorage.setItem('mode', 'sun')
       }
-      
    }
+   onMount(async()=>{
+      setTimeout(() => {
+         if(window.localStorage.getItem('mode') == 'moon'){
+            document.getElementsByClassName('map-tiles-container')[0].classList.add('map-tiles')
+            icon = 'sun'
+         } else{
+            document.getElementsByClassName('map-tiles-container')[0].classList.remove('map-tiles')
+            icon = 'moon'
+         }
+      }, 700);
+      
+      
+   })
 </script>
 
 <div class="navbar-container">
@@ -30,11 +45,11 @@
       <div class="left-block">
          <a class="nav-title button nav-link icon" href="/">{@html feather.icons.activity.toSvg()} Earthquakes v2</a>
       </div>
-   <div class="rigth-block">
+      <div class="rigth-block">
          <a on:click={switchMode} href="/#" class="button-icon nav-link icon" >{@html feather.icons[icon].toSvg()}</a>
          <a on:click={()=>{getData(true)}} href="/#" class="button-icon nav-link icon" >{@html feather.icons['refresh-cw'].toSvg()}</a>
          <a href="https://github.com/alombi/earthquakes" target="_blank" class="button-icon nav-link icon">{@html feather.icons.github.toSvg()}</a>
-   </div>
+      </div>
    </nav>
 </div>
 
